d68dbcc76344bff60d217ce771a9f28ca5945a94
5ub34x <38159182+5ub34x@users.noreply.github.com>
2018-04-15 16:52:38 +0100

    Update query_whois.py
    Added exception handling when match not found.

--- chap1/query_whois.py
+++ chap1/query_whois.py
@@ -23,13 +23,16 @@ with open(sys.argv[1], "r") as ins:
         p = Popen(['whois', ip_address], stdin=PIPE, stdout=PIPE, stderr=PIPE)
         output, err = p.communicate(b"input data that is passed to subprocess' stdin")
         
-        rc = p.returncode
-        output = output.lower()
-        if rc == 0:
-            netname =  catch_word("netname:", output).strip()
-            country =  catch_word("country:", output).strip()
-            if "netrange" in output:
-                inetnum = catch_word("netrange:", output).strip()
-            elif "inetnum" in output:
-                inetnum = catch_word("inetnum:", output).strip()
-            print line.strip() +","+ netname +","+ inetnum +","+ country
+        try:
+            rc = p.returncode
+            output = output.lower()
+            if rc == 0:
+                netname =  catch_word("netname:", output).strip()
+                country =  catch_word("country:", output).strip()
+                if "netrange" in output:
+                    inetnum = catch_word("netrange:", output).strip()
+                elif "inetnum" in output:
+                    inetnum = catch_word("inetnum:", output).strip()
+                print line.strip() +","+ netname +","+ inetnum +","+ country
+        except:
+            continue
